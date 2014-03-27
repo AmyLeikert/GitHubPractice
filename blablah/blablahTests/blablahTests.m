a47f7ef364dfae0e89d18c4fc04cbb99dd46aab1
Amy Leikert <amy.leikert@detroitlabs.com>
2014-03-27 11:26:10 -0400

    practice
    blahblah

---
+++ blablah/blablahTests/blablahTests.m
@@ -0,0 +1,34 @@
+//
+//  blablahTests.m
+//  blablahTests
+//
+//  Created by DetroitLabsUser on 3/27/14.
+//  Copyright (c) 2014 DetroitLabsUser. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface blablahTests : XCTestCase
+
+@end
+
+@implementation blablahTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
